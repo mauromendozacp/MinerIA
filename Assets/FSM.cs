5ba1ab797f0b9dbdf0d255a526350e87e2d0d245
Mauro <mauro.mendozacp@gmail.com>
2022-08-18 18:57:15 -0300

    Initial Commit

---
+++ Assets/FSM.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class FSM
+{
+    private int currentState;
+    private int[,] relations;
+    private Dictionary<int, List<Action>> behaviours;
+
+    public FSM(int states, int flags)
+    {
+        currentState = -1;
+
+        relations = new int[states, flags];
+        for (int i = 0; i < states; i++)
+            for (int j = 0; j < flags; j++)
+                relations[i, j] = -1;
+
+        behaviours = new Dictionary<int, List<Action>>();
+    }
+
+    public void ForceCurretState(int state)
+    {
+        currentState = state;
+    }
+
+    public void SetRelation(int sourceState, int flag, int destinationState)
+    {
+        relations[sourceState, flag] = destinationState;
+    }
+
+    public void SetFlag(int flag)
+    {
+        if (relations[currentState, flag] != -1)
+            currentState = relations[currentState, flag];
+    }
+
+    public int GetCurrentState()
+    {
+        return currentState;
+    }
+
+    public void SetBehaviour(int state, Action behaviour)
+    {
+        List<Action> newBehaviours = new List<Action>();
+        newBehaviours.Add(behaviour);
+
+        if (behaviours.ContainsKey(state))
+            behaviours[state] = newBehaviours;
+        else
+            behaviours.Add(state, newBehaviours);
+    }
+
+    public void AddBehaviour(int state, Action behaviour)
+    {
+
+        if (behaviours.ContainsKey(state))
+            behaviours[state].Add(behaviour);
+        else
+        {
+            List<Action> newBehaviours = new List<Action>();
+            newBehaviours.Add(behaviour);
+            behaviours.Add(state, newBehaviours);
+        }
+    }
+
+    public void Update()
+    {
+        if (behaviours.ContainsKey(currentState))
+        {
+            List<Action> actions = behaviours[currentState];
+            if (actions != null)
+            {
+                for (int i = 0; i < actions.Count; i++)
+                {
+                    if (actions[i] != null)
+                    {
+                        actions[i].Invoke();
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Miner.cs
@@ -0,0 +1,109 @@
+﻿using UnityEngine;
+
+public class Miner : MonoBehaviour
+{
+    #region PRIVATE_FIELDS
+    private GameObject mine;
+    private GameObject deposit;
+    private FSM fsm;
+
+    private int mineUses = 10;
+    private float miningTime = 5.0f;
+    private float currentMiningTime = 0.0f;
+    #endregion
+
+    #region ENUMS
+    enum States
+    {
+        Mining,
+        GoToMine,
+        GoToDeposit,
+        Idle,
+
+        _Count
+    }
+
+    enum Flags
+    {
+        OnFullInventory,
+        OnReachMine,
+        OnReachDeposit,
+        OnEmpyMine,
+
+        _Count
+    }
+    #endregion
+
+    #region PUBLIC_METHODS
+    public void Init(GameObject mine, GameObject deposit)
+    {
+        this.mine = mine;
+        this.deposit = deposit;
+    }
+
+    public void StartMiner()
+    {
+        fsm = new FSM((int)States._Count, (int)Flags._Count);
+        fsm.ForceCurretState((int)States.GoToMine);
+
+        fsm.SetRelation((int)States.GoToMine, (int)Flags.OnReachMine, (int)States.Mining);
+        fsm.SetRelation((int)States.Mining, (int)Flags.OnFullInventory, (int)States.GoToDeposit);
+        fsm.SetRelation((int)States.GoToDeposit, (int)Flags.OnReachDeposit, (int)States.GoToMine);
+        fsm.SetRelation((int)States.GoToDeposit, (int)Flags.OnEmpyMine, (int)States.Idle);
+
+        fsm.AddBehaviour((int)States.Idle, () => { Debug.Log("Idle"); });
+
+        fsm.AddBehaviour((int)States.Mining, () =>
+        {
+            if (currentMiningTime < miningTime)
+            {
+                currentMiningTime += Time.deltaTime;
+            }
+            else
+            {
+                currentMiningTime = 0.0f;
+                fsm.SetFlag((int)Flags.OnFullInventory);
+                mineUses--;
+            }
+        });
+
+        fsm.AddBehaviour((int)States.GoToMine, () =>
+        {
+            Vector2 dir = (mine.transform.position - transform.position).normalized;
+
+            if (Vector2.Distance(mine.transform.position, transform.position) > 1.0f)
+            {
+                Vector2 movement = dir * 10.0f * Time.deltaTime;
+                transform.position += new Vector3(movement.x, movement.y);
+            }
+            else
+            {
+                fsm.SetFlag((int)Flags.OnReachMine);
+            }
+        });
+
+        fsm.AddBehaviour((int)States.GoToDeposit, () =>
+        {
+            Vector2 dir = (deposit.transform.position - transform.position).normalized;
+
+            if (Vector2.Distance(deposit.transform.position, transform.position) > 1.0f)
+            {
+                Vector2 movement = dir * 10.0f * Time.deltaTime;
+                transform.position += new Vector3(movement.x, movement.y);
+            }
+            else
+            {
+                if (mineUses <= 0)
+                    fsm.SetFlag((int)Flags.OnEmpyMine);
+                else
+                    fsm.SetFlag((int)Flags.OnReachDeposit);
+            }
+        });
+    }
+
+    public void UpdateMiner()
+    {
+        fsm.Update();
+    }
+    #endregion
+}
